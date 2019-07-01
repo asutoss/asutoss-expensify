532edc693093edc2b43ba7eb95df0fde85ff7da5
AsUToss <ashutoshpal0456@gmail.com>
2019-07-01 23:42:41 +0530

    add expensify styles

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -56,38 +56,42 @@ export default class ExpenseForm extends React.Component {
     }
     render() {
         return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.onSubmit}>
-                    <input  
-                        type='text'
-                        placeholder='Description'
-                        autoFocus
-                        value={this.state.description}
-                        onChange={this.onDescriptionChange}
-                    />
-                    <input  
-                        type='text'
-                        value={this.state.amount}
-                        placeholder='Amount'
-                        onChange={this.onAmountChange}
-                    />
-                    <SingleDatePicker 
+            <form className='form' onSubmit={this.onSubmit}>
+                {this.state.error && <p className='form__error'>{this.state.error}</p>}
+                <input  
+                    type='text'
+                    placeholder='Description'
+                    autoFocus
+                    className='text-input'
+                    value={this.state.description}
+                    onChange={this.onDescriptionChange}
+                />
+                <input  
+                    type='text'
+                    className='text-input'
+                    value={this.state.amount}
+                    placeholder='Amount'
+                    onChange={this.onAmountChange}
+                />
+                <SingleDatePicker 
                     date={this.state.createdAt}
                     onDateChange={this.onDateChange}
                     focused={this.state.calendarFocused}
                     onFocusChange={this.onFocusChange}
                     numberOfMonths={1}
                     isOutsideRange={() => false }
-                    />
-                    <textarea 
-                        placeholder='Add a note for your expense {optional}'
-                        value={this.state.note}
-                        onChange={this.onNoteChange}
-                    />
-                    <button>Add Expense</button>
-                </form>
-            </div>
+                    block
+                />
+                <textarea
+                    className='textarea' 
+                    placeholder='Add a note for your expense {optional}'
+                    value={this.state.note}
+                    onChange={this.onNoteChange}
+                />
+                <div>
+                    <button className='button'>Save Expense</button>
+                </div>
+            </form>
         )
     }
 }
